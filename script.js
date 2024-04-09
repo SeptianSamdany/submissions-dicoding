a304771fe25a0a12443ed92c7f9265fdc8f27025
SeptianSamdany <166478644+SeptianSamdany@users.noreply.github.com>
2024-04-09 13:00:19 +0700

    Submissions Belajar Pemrograman Dasar Dicoding

---
+++ script.js
@@ -0,0 +1,10 @@
+// Toggle Class Active
+
+const navbarNav = document.querySelector('.navbar-nav')
+
+// Ketika Hamburger Menu Di Klik 
+
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active'); 
+};
+
